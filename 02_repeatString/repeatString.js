92095e1653a15ca963ad1b9f35497a0b08d1f0ae
Nakura <126201955+nakurahe@users.noreply.github.com>
2024-10-10 13:35:03 -0700

    Completed 01, 02, and 12

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
+const repeatString = (string, times) => {
+    if (times < 0) {
+        return 'ERROR';
+    }
+    return string.repeat(times);
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,32 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    people.sort((a, b) => {
+        if (a.yearOfDeath == undefined) {
+            yearOfDeathA = 2024;
+        } else {
+            yearOfDeathA = a.yearOfDeath;
+        }
+        const ageOfA = yearOfDeathA - a.yearOfBirth;
 
+        if (b.yearOfDeath == undefined) {
+            yearOfDeathB = 2024;
+        } else {
+            yearOfDeathB = b.yearOfDeath;
+        }
+        const ageOfB = yearOfDeathB - b.yearOfBirth;
+
+        if (ageOfA > ageOfB) return -1;
+        if (ageOfA < ageOfB) return 1;
+        return 0;
+    })
+    console.log(people);
+    return people[0];
+
+    // Alternative solution:
+    // return people.sort((a, b) => {
+    //     const lastPerson = (a.yearOfDeath || new Date().getFullYear()) - a.yearOfBirth;
+    //     const nextPerson = (b.yearOfDeath || new Date().getFullYear()) - b.yearOfBirth;
+    //     return lastPerson > nextPerson ? -1 : 1;
+    // })[0];
 };
 
 // Do not edit below this line
